fcc18b200dd74bf9011aaffb2097b66019e07350
Katsudon27 <katsudon1127@gmail.com>
2026-05-07 12:24:13 +1000

    Add method to check if code guesser won

--- lib/game.rb
+++ lib/game.rb
@@ -7,15 +7,23 @@ class Game
     @code_maker = CodeMaker.new(self)
     @code_guesser = CodeGuesser.new(self)
     @game_board = GameBoard.new()
+    @number_of_guesses = 0
   end
 
   def evaluate_guess(guess, answer)
     feedback = []
+    if check_win?(guess, answer)
+      return ["1", "1", "1", "1"]
+    end
     guess.each_with_index do |digit, index|
       if answer.include?(digit)
         answer.index(digit) == index ? feedback << "1" : feedback << "2"
       end
     end
     feedback
+  end
+
+  def check_win?(guess, answer)
+    guess == answer ? true : false
   end
 end
